trying to fix backpropagation

mcts.py
@@ -46,7 +46,7 @@ class Node:
         if self.visit_count == 0:
             return np.inf
 
-        return self.mean_action_value + 2 * np.sqrt(np.log(self.parent_node.visit_count) / self.visit_count)
+        return self.mean_action_value + np.sqrt(2) * np.sqrt(np.log(self.parent_node.visit_count) / self.visit_count)
 
     def select_child_node(self) -> "Node":
         selected_child_node = sorted(self.child_nodes,
@@ -129,11 +129,14 @@ class MCTS:
             # Indeed, if one player wins the roll-out, then only the nodes that are *about* to make
             #   the player's move ought to be rewarded
             if winner != "drawn":
-                winner_turn = 1 if winner == "white" else 0
+                winner_turn = ConnectFour.WHITE if winner == "white" else ConnectFour.BLACK
 
-                if current_node.state.turn == winner_turn:
+                if current_node.state.turn != winner_turn:
                     current_node.total_action_value += 1
-                    current_node.mean_action_value = current_node.total_action_value / current_node.visit_count
+                # else:
+                #     current_node.total_action_value -= 1
+
+                current_node.mean_action_value = current_node.total_action_value / current_node.visit_count
 
             current_node = current_node.parent_node
 
@@ -187,12 +190,12 @@ def main():
         "drawn": 0
     }
 
-    for i in range(3):
+    for i in range(1):
         print(i)
         sample_state = ConnectFour()
 
-        white_mcts = MCTS(root_state=sample_state, player=ConnectFour.WHITE, itermax=1000, timeout_s=40000000000)
-        black_mcts = MCTS(root_state=sample_state, player=ConnectFour.BLACK, itermax=1000, timeout_s=40000000000)
+        # white_mcts = MCTS(root_state=sample_state, player=ConnectFour.WHITE, itermax=1000, timeout_s=40000000000)
+        # black_mcts = MCTS(root_state=sample_state, player=ConnectFour.BLACK, itermax=1000, timeout_s=40000000000)
 
         while not sample_state.game_over:
             sample_state.print_board()
@@ -202,16 +205,18 @@ def main():
                 move = random.choice(sample_state.legal_moves)
                 # move = int(input("Your turn: "))
                 # print("After : ", white_mcts.root_node)
+                # white_mcts = MCTS(root_state=sample_state, player=ConnectFour.WHITE, itermax=10, timeout_s=40000000000)
                 # move = white_mcts.get_best_move()
 
                 # Update the MCTS for the opponent
                 black_mcts.make_opponent_move(move)
             else:
+                black_mcts = MCTS(root_state=sample_state, player=ConnectFour.BLACK, itermax=1000, timeout_s=40000000000)
                 # move = random.choice(sample_state.legal_moves)
                 # move = int(input("Your Move: "))
                 move = black_mcts.get_best_move()
 
-                white_mcts.make_opponent_move(move)
+                # white_mcts.make_opponent_move(move)
 
             sample_state.make_move(move)
 